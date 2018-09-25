2445aac2a5f89d2dcd26e95fe40020f5181a88b8
v470 <byu1510@bths.edu>
2018-09-24 21:24:19 -0400

    up

--- src/BYu/BYuLib.java
+++ src/BYu/BYuLib.java
@@ -8,24 +8,57 @@ public class BYuLib
     {
         System.out.println(str);
     }
-    public static boolean isPalindrome(String str)
+    public static String cutOut(String mainStr, String subStr)
     {
-        str.toLowerCase();
-        for (int i = str.length("")- 1; i > 0; i -- )
+        return(mainStr.replaceFirst(subStr,""));
+    }
+    //replaces first instance of subStr in mainStr with a blank space
+    public static String sumUpTo(int sum)
+    {
+        int asf = 0;
+        for(int i = sum; i > 0; i--)
         {
-            String sub = str.substring(i,i++);
+            asf += i;
         }
 
+        return String.valueOf(asf);
     }
+    //as long as i is equal to the sum i will get subtracted by one
+    //then the int asf will add that integer, this will continue until
+    //i becomes less than zero, in the end it will put out an integer
+    // that is the sum from zero to the input
+    public static void multiplicationTable(int base, int range)
+    {
+        int asf = 0;
+        for(int i = 0; i <= range; i++)
+        {
+            System.out.println(asf);
+            asf = base + asf;
+        }
+    }
+    public static void  fooBarBaz(int num1)
+    {
+        for(int i = 1; i <= num1; i++)
+        {
+            if (i%3 == 0&& i%5 != 0)
+            {
+                System.out.println("foo");
+            }
+            if (i%3 != 0&& i%5 == 0)
+            {
+                System.out.println("bar");
+            }
+            if (i%3 == 0&& i%5 == 0)
+            {
+                System.out.println("baz");
+            }
+            if (i%3 != 0&& i%5 != 0)
+            System.out.println(String.valueOf(i));
+        }
+    }
+
 
 
 }
 
-/*
-String Methods
-isPalindrome/dateStr
-Math Methods
-multiplicationTable/sumUpTo
-Extra
-cutOut
- */
+
--- src/BYu/TestFile.java
+++ src/BYu/TestFile.java
@@ -1,12 +1,16 @@
 //Brian Yu
 package BYu;
 
-import static BYu.BYuLib.println;
+import static BYu.BYuLib.*;
 
 public class TestFile {
 
     public static void main(String[] args)
     {
         println("heck");
+        println(cutOut("catatonic cat", "cat"));
+        println(sumUpTo(12));
+        multiplicationTable(4,11);
+        fooBarBaz(9);
     }
 }
